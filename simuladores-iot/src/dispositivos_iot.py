994f85540f6dbe7a67924556db6e61cf4f8962a8
unknown <isylucywyn@outlook.com>
2026-08-07 22:09:26 -0300

    fix: melhoria no payload e adição do status 'ligado' dos iot

--- simuladores-iot/src/dispositivos_iot.py
+++ simuladores-iot/src/dispositivos_iot.py
@@ -16,56 +16,56 @@ dispositivos = {
     "servidor": {
         "id_dispositivo": "servidor_sistema",
         "watts": 150.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 1,
         "categoria": "Critico",
     },
     "sensor_temperatura": {
         "id_dispositivo": "sensor_temperatura",
         "watts": 1.5,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 1,
         "categoria": "Critico",
     },
     "cameras_seguranca": {
         "id_dispositivo": "cameras_seguranca",
         "watts": 25.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 1,
         "categoria": "Critico",
     },
     "luzes_seguranca": {
         "id_dispositivo": "luzes_seguranca",
         "watts": 40.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 1,
         "categoria": "Critico",
     },
     "modem": {
         "id_dispositivo": "modem",
         "watts": 12.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 1,
         "categoria": "Critico",
     },
     "geladeira": {
         "id_dispositivo": "geladeira",
         "watts": 150.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 2,
         "categoria": "Essencial",
     },
     "ventilador": {
         "id_dispositivo": "ventilador",
         "watts": 65.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 3,
         "categoria": "Importante",
     },
     "alexa": {
         "id_dispositivo": "alexa",
         "watts": 3.0,
-        "ativo": True,
+        "ligado": True,
         "prioridade": 4,
         "categoria": "Secundario",
     },
@@ -85,11 +85,11 @@ def on_message(client, userdata, msg):
         for dev_id, info in dispositivos.items():
             if info["categoria"] == categoria_alvo:
                 encontrou_alguem = True
-                if comando == "DESLIGAR" and info["ativo"]:
-                    info["ativo"] = False
+                if comando == "DESLIGAR" and info["ligado"]:
+                    info["ligado"] = 0
                     print(f"🛑 [IoT-Comando] Corte de Energia: Desligando {dev_id} (Categoria: {categoria_alvo}).")
-                elif comando == "LIGAR" and not info["ativo"]:
-                    info["ativo"] = True
+                elif comando == "LIGAR" and not info["ligado"]:
+                    info["ligado"] = 1
                     print(f"🟢 [IoT-Comando] Energia Restaurada: Religando {dev_id} (Categoria: {categoria_alvo}).")
                 
     except Exception as e:
@@ -126,14 +126,18 @@ try:
         for dev, info in dispositivos.items():
 
             # Se o dispositivo estiver ativo, envia a potência real. Se não, envia 0W.
-            consumo_atual = info["watts"] if info["ativo"] else 0.0
+            consumo_atual = info["watts"] if info["ligado"] else 0.0
             payload = {
+                "id_dispositivo": info["id_dispositivo"], 
+                "categoria": info["categoria"],           
+                "ligado": info["ligado"],                   
+                "prioridade": info["prioridade"],         
                 "watts": consumo_atual,
                 "timestamp": timestamp_atual
             }
 
             # Monta o tópico dinâmico: iot/dispositivos/geladeira/consumo, etc.
-            topico_dinamico = f"iot/dispositivos/{dev}/consumo"
+            topico_dinamico = f"iot/dispositivos/{dev}/consumo"   
             client.publish(topico_dinamico, json.dumps(payload))
 
         time.sleep(1)
